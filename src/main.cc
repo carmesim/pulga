6680e1ffafc1ff9a1131313040052afd687c4a0c
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2020-08-03 21:33:56 -0300

    Hopefully decrease IO time

--- src/main.cc
+++ src/main.cc
@@ -19,15 +19,18 @@ static inline void print_info(){
     const char * color1 = "\033[0;35m";
     const char * color3 = "\033[1;32m";
     unsigned char max_width = 12;
-    printf("%s%-*s> %s%s\n", color1, max_width, "username",   color3, ExoFetch::get_username().c_str());
-    printf("%s%-*s> %s%s\n", color1, max_width, "hostname",   color3, ExoFetch::get_hostname().c_str());
-    printf("%s%-*s> %s%s\n", color1, max_width, "os",         color3, ExoFetch::get_distro().c_str());
-    printf("%s%-*s> %s%s\n", color1, max_width, "kernel",     color3, ExoFetch::get_kernel().c_str());
-    printf("%s%-*s> %s%s\n", color1, max_width,"system arch", color3, ExoFetch::get_arch().c_str());
-    printf("%s%-*s> %s%s\n", color1, max_width, "uptime",     color3, ExoFetch::get_uptime().c_str());
-    printf("%s%-*s> %s%s\n", color1, max_width,"packages",    color3, ExoFetch::get_packages().c_str());
-    printf("%s%-*s> %s%s\n", color1, max_width, "shell",      color3, ExoFetch::get_shell().c_str());
-    printf("%s%-*s> %s%s\n", color1, max_width, "cpu",        color3, ExoFetch::get_cpu_model().c_str());
+    char buffer[2048];
+    int length = 0;
+    length += sprintf(buffer, "%s%-*s> %s%s\n", color1, max_width, "username",   color3, ExoFetch::get_username().c_str());
+    length += sprintf(buffer + length, "%s%-*s> %s%s\n", color1, max_width, "hostname",   color3, ExoFetch::get_hostname().c_str());
+    length += sprintf(buffer + length, "%s%-*s> %s%s\n", color1, max_width, "os",         color3, ExoFetch::get_distro().c_str());
+    length += sprintf(buffer + length, "%s%-*s> %s%s\n", color1, max_width, "kernel",     color3, ExoFetch::get_kernel().c_str());
+    length += sprintf(buffer + length, "%s%-*s> %s%s\n", color1, max_width,"system arch", color3, ExoFetch::get_arch().c_str());
+    length += sprintf(buffer + length, "%s%-*s> %s%s\n", color1, max_width, "uptime",     color3, ExoFetch::get_uptime().c_str());
+    length += sprintf(buffer + length, "%s%-*s> %s%s\n", color1, max_width,"packages",    color3, ExoFetch::get_packages().c_str());
+    length += sprintf(buffer + length, "%s%-*s> %s%s\n", color1, max_width, "shell",      color3, ExoFetch::get_shell().c_str());
+    length += sprintf(buffer + length, "%s%-*s> %s%s\n", color1, max_width, "cpu",        color3, ExoFetch::get_cpu_model().c_str());
+    puts(buffer);
 }
 
 int main()
