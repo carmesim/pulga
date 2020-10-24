7272633c9dbec17b8d4c8fcca04c69ddc450ea54
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2020-10-24 01:22:47 -0300

    Add memory usage info

--- src/exofetch.rs
+++ src/exofetch.rs
@@ -1,5 +1,6 @@
 use std::{env, fs::File, collections::HashMap, io::{self, Read}};
 use std::{path::PathBuf, mem, ptr, str};
+use std::cmp;
 use std::ffi::{CStr, OsString};
 use libc::{getuid, getpwuid_r, passwd};
 use whoami;
@@ -26,17 +27,19 @@ pub struct UserData
     pub username:   String,     // User's username
     pub hostname:   String,     // User's hostname
     pub devicename: String,     // User's device name
-    pub cwd:        String,     // User's current working directory. TODO: unneeded?
-    pub hmd:        String,     // User's home directory
+    pub cwd:        PathBuf,    // User's current working directory. TODO: unneeded?
+    pub hmd:        PathBuf,    // User's home directory
     pub desk_env:   String,     // User's desktop environment
     pub distro:     String,     // User's distro
     pub platform:   String,     // User's platform 
-    pub total_memory: u64,      //
+    pub total_memory: String,   // Total memory in human-readable form
+    pub used_memory:  String    // Used memory in human-readable form
 }
 
 #[repr(C)]
 #[derive(Debug)]
-pub struct MemInfo {
+pub struct MemInfo 
+{
     /// Total physical memory.
     pub total:   u64,
     pub free:    u64,
@@ -50,19 +53,23 @@ pub struct MemInfo {
     pub swap_free:  u64,
 }
 
-//pub fn get_home_dir() -> Result<String, Error>
+/// Returns the user's home directory
 pub fn home_dir() -> Option<PathBuf> 
 {
+    // Returns the home directory as specified by the HOME ..
+    // .. directory 
+
     return std::env::var_os("HOME").or_else(|| unsafe {
         let mut buf = Vec::with_capacity(2048);
         let mut passwd: passwd = mem::zeroed();
         let mut result = ptr::null_mut();
-        match getpwuid_r(getuid(), &mut passwd, buf.as_mut_ptr(),
-                                buf.capacity(), &mut result) {
-            0 if !result.is_null() => {
+        match getpwuid_r(getuid(), &mut passwd, buf.as_mut_ptr(), buf.capacity(), &mut result) 
+        {
+            0 if !result.is_null() => 
+            {
                 let ptr = passwd.pw_dir as *const _;
                 let bytes = CStr::from_ptr(ptr).to_bytes().to_vec();
-                Some(OsString::from(str::from_utf8(&bytes).unwrap().to_string()))
+                Some(OsString::from(str::from_utf8(&bytes).unwrap()))
             },
             _ => None,
         }
@@ -70,7 +77,9 @@ pub fn home_dir() -> Option<PathBuf>
 }
 
 // This function was adapted from sys-info by Siyu Wang (MIT-licensed)
-pub fn mem_info() -> Result<MemInfo, Error> {
+/// Returns current memory utilization info
+pub fn mem_info() -> Result<MemInfo, Error> 
+{
     let mut s = String::new();
     File::open("/proc/meminfo")?.read_to_string(&mut s)?;
     let mut mem_info_map = HashMap::new();
@@ -110,32 +119,50 @@ pub fn mem_info() -> Result<MemInfo, Error> {
        })
 }
 
+/// pretty_bytes gets a value in bytes and returns a human-readable form of it
+fn pretty_bytes(num: f64) -> String {
+    let negative = if num > 0_f64 { "" } else { "-" };
+    let num = num.abs();
+    const UNITS: &[&str] = &["B", "kB", "MB", "GB", "TB"];
+    if num < 1_f64 
+    {
+        return format!("{}{} {}", negative, num, "B");
+    }
+    let v1 = (num.ln() / 1000_f64.ln()).floor() as i32;
+    let exponent = cmp::min(v1, 4_i32);
+    let pretty_bytes = format!("{:.2}", num / 1000_f64.powi(exponent)).parse::<f64>().unwrap();
+    let unit:&str = UNITS[exponent as usize];
+    format!("{}{} {}", negative, pretty_bytes, unit)
+  }
+
 /// get_user_data returns a new UserData structure
 pub fn get_user_data() -> UserData
 {
     // Current working directory
     let cwd = env::current_dir().unwrap();
-    let cwd_str: String = cwd.as_os_str().to_str().unwrap().to_string();
-    drop(cwd);
+    // let cwd_str: String = cwd.as_os_str().to_str().unwrap().to_string();
+    // drop(cwd);
 
     // Home directory
-    // let hmd = dirs::home_dir().unwrap();
     let hmd = home_dir().unwrap();
-    let hmd_str: String = hmd.as_os_str().to_str().unwrap().to_string();
-    drop(hmd);
+    // let hmd_str: String = hmd.as_os_str().to_str().unwrap().to_string();
+    // drop(hmd);
     
     let mem_info = mem_info().unwrap();
+
+    println!("{:?}", mem_info);
 
     return UserData
     {
         username:   whoami::username(),
         hostname:   whoami::hostname(),
         devicename: whoami::devicename(),
-        cwd:        cwd_str,
-        hmd:        hmd_str,
+        cwd:        cwd,
+        hmd:        hmd,
         desk_env:   whoami::desktop_env().to_string(),
         distro:     whoami::distro(),
         platform:   whoami::platform().to_string(),
-        total_memory: mem_info.total,
+        total_memory: pretty_bytes((mem_info.total * 1024) as f64),
+        used_memory:  pretty_bytes(((mem_info.total - mem_info.avail)*1024) as f64)
     };
 }
