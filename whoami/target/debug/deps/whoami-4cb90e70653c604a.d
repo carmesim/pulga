e9a5cb9546434401ecde4be9c3d1caae1f39c44b
João M. Bezerra <marcospb19@hotmail.com>
2020-12-06 13:04:02 -0300

    Updating .gitignore and removing target/

--- whoami/target/debug/deps/whoami-4cb90e70653c604a.d
+++
@@ -1,6 +0,0 @@
-/media/vinicius/82913e6d-e865-4017-a067-91ae15c0af281/exofetch-rs/whoami/target/debug/deps/whoami-4cb90e70653c604a.rmeta: src/lib.rs src/unix.rs
-
-/media/vinicius/82913e6d-e865-4017-a067-91ae15c0af281/exofetch-rs/whoami/target/debug/deps/whoami-4cb90e70653c604a.d: src/lib.rs src/unix.rs
-
-src/lib.rs:
-src/unix.rs:
