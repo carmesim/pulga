6171080a8f8eeab8a354c042156ae4dac2516c38
João M. Bezerra <marcospb19@hotmail.com>
2020-12-06 21:55:12 -0300

    Using termion instead of colored, starting logos

---
+++ src/logos.rs
@@ -0,0 +1,50 @@
+use indoc::indoc;
+
+pub enum Logo {
+    Arch,
+    Manjaro,
+}
+
+pub fn choose_logo(logo: Logo) -> &'static str {
+    match logo {
+        Logo::Arch => ARCH_LOGO,
+        Logo::Manjaro => MANJARO_LOGO,
+    }
+}
+
+const ARCH_LOGO: &'static str = indoc! {"
+    {c}                  -`
+    {c}                 .o+`
+    {c}                `ooo/
+    {c}               `+oooo:
+    {c}              `+oooooo:
+    {c}              -+oooooo+:
+    {c}            `/:-:++oooo+:
+    {c}           `/++++/+++++++:
+    {c}          `/++++++++++++++:
+    {c}         `/+++ooooooooooooo/`
+    {c}        ./ooosssso++osssssso+`
+    {c}       .oossssso-````/ossssss+`
+    {c}      -osssssso.      :ssssssso.
+    {c}     :osssssss/        osssso+++.
+    {c}    /ossssssss/        +ssssooo/-
+    {c}  `/ossssso+/:-        -:/+osssso+-
+    {c} `+sso+:-`                 `.-/+oso:
+    {c}`++:.                           `-/+/
+    {c}.`                                 `/"};
+
+const MANJARO_LOGO: &'static str = indoc! {"
+    {g}██████████████████  ████████
+    {g}██████████████████  ████████
+    {g}██████████████████  ████████
+    {g}██████████████████  ████████
+    {g}████████            ████████
+    {g}████████  ████████  ████████
+    {g}████████  ████████  ████████
+    {g}████████  ████████  ████████
+    {g}████████  ████████  ████████
+    {g}████████  ████████  ████████
+    {g}████████  ████████  ████████
+    {g}████████  ████████  ████████
+    {g}████████  ████████  ████████
+    {g}████████  ████████  ████████"};
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,75 @@
 mod error;
-mod logo;
+mod logos;
 mod newfetch;
 
-use colored::Colorize; // This trait allows us to use colors on Strings and &strs
+use crate::newfetch::UserData;
+
+use indoc::indoc;
+use sugars::{boxed, hmap};
+use termion::color::*;
+
+use std::collections::HashMap;
+
+fn show_logo() {
+    let mut color_map: HashMap<char, Box<dyn Color>> = hmap! {};
+    color_map.insert('k', boxed!(Black));
+    color_map.insert('b', boxed!(Blue));
+    color_map.insert('c', boxed!(Cyan));
+    color_map.insert('g', boxed!(Green));
+    color_map.insert('m', boxed!(Magenta));
+    color_map.insert('r', boxed!(Red));
+    color_map.insert('R', boxed!(Reset));
+    color_map.insert('w', boxed!(White));
+    color_map.insert('y', boxed!(Yellow));
+
+    // k // Black
+    // b // Blue
+    // c // Cyan
+    // g // Green
+    // m // Magenta
+    // r // Red
+    // R // Reset   Reset colors to defaults.
+    // w // White
+    // y // Yellow
+    // Ignore for now?
+    // LightBlack  High-intensity light black.
+    // LightBlue   High-intensity light blue.
+    // LightCyan   High-intensity light cyan.
+    // LightGreen  High-intensity light green.
+    // LightMagenta    High-intensity light magenta.
+    // LightRed    High-intensity light red.
+    // LightWhite  High-intensity light white.
+    // LightYellow High-intensity light yellow.
+}
 
 fn main() {
-    let user_data = newfetch::get_user_data();
-    println!("{}: {}", "username".cyan(), user_data.username.red());
-    println!("{}: {}", "hostname".cyan(), user_data.hostname.red());
-    println!("{}: {}", "device name".cyan(), user_data.devicename.red());
-    println!("{}: {}", "home dir.".cyan(), user_data.hmd.red());
-    println!("{}: {}", "platform".cyan(), user_data.platform.red());
-    println!("{}: {}", "distro".cyan(), user_data.distro.red());
-    println!("{}: {}", "desktop env.".cyan(), user_data.desk_env.red());
-    println!(
-        "{}: {}/{}",
-        "memory usage".cyan(),
-        user_data.used_memory.red(),
-        user_data.total_memory.red()
+    let data: UserData = newfetch::get_user_data();
+
+    #[rustfmt::skip]
+    println!(indoc! {
+        "{c}{}{w}: {r}{}{R}
+         {c}{}{w}: {r}{}{R}
+         {c}{}{w}: {r}{}{R}
+         {c}{}{w}: {r}{}{R}
+         {c}{}{w}: {r}{}{R}
+         {c}{}{w}: {r}{}{R}
+         {c}{}{w}: {r}{}{R}
+         {c}{}{w}: {r}{}{R}/{r}{}{R}"
+        },
+        "username", data.username,
+        "hostname", data.hostname,
+        "device name", data.devicename,
+        "home dir.", data.hmd,
+        "platform", data.platform,
+        "distro", data.distro,
+        "desktop env.", data.desk_env,
+        "memory usage", data.used_memory, data.total_memory,
+        c = Fg(LightCyan),
+        w = Fg(LightBlack),
+        R = Fg(Reset),
+        r = Fg(LightRed)
     );
 
-    println!("{}", logo::ARGH_LOGO);
+    let logo = logos::choose_logo(logos::Logo::Manjaro);
+    println!("{}", logo);
 }
