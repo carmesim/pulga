e987a2bf6c1b63c6238e342b839c4a31ce5cbe42
João M. Bezerra <marcospb19@hotmail.com>
2020-12-19 12:52:33 -0300

    Starting colored logo display

--- src/main.rs
+++ src/main.rs
@@ -13,41 +13,59 @@ use std::collections::HashMap;
 fn show(text: String, logo: &str) {
     let lines: Vec<String> = text.lines().map(|x| x.trim().to_string()).collect();
 
-    // Logo tem 14 de largura hmm
-    println!("{}", logo);
+    // Code to show colored logo
+    #[rustfmt::skip]
+    let color_map = {
+        let mut m: HashMap<char, Box<dyn Color>> = hmap! {};
+        m.insert('k', boxed!(Black  )); // k => Black
+        m.insert('b', boxed!(Blue   )); // b => Blue
+        m.insert('c', boxed!(Cyan   )); // c => Cyan
+        m.insert('g', boxed!(Green  )); // g => Green
+        m.insert('m', boxed!(Magenta)); // m => Magenta
+        m.insert('r', boxed!(Red    )); // r => Red
+        m.insert('w', boxed!(White  )); // w => White
+        m.insert('y', boxed!(Yellow )); // y => Yellow
+        m.insert('R', boxed!(Reset  )); // R => Reset all
+        m
+    };
+
+    let logo = logo.chars().collect::<Vec<char>>();
+
+    let mut i = 0;
+    while i < logo.len() - 2 {
+        let slice = &logo[i..=i + 2];
+
+        match slice {
+            ['{', color_id, '}'] => {
+                let color: &dyn Color = match color_map.get(color_id) {
+                    None => panic!("Unexpected color_id '{}'", color_id),
+                    Some(color) => color.as_ref(),
+                };
+
+                print!("{}", Fg(color));
+                i += 3;
+            },
+            [first, ..] => {
+                print!("{}", *first as char);
+                i += 1;
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    // Show the remaining stuff
+    for i in i..logo.len() {
+        print!("{}", logo[i]);
+    }
+    println!();
+
+    // Code to insert information at the side of the logo
     print!("{}", Up(14));
 
     for (_, line) in lines.iter().enumerate() {
         print!("{} {}{}{}", Right(31), line, Left(1000), Down(1));
     }
-
-    print!("{}", Down(17));
-
-    #[rustfmt::skip]
-    let _color_map = {
-        let mut m: HashMap<char, Box<dyn Color>> = hmap! {};
-        m.insert('k', boxed!(Black));  // k // Black
-        m.insert('b', boxed!(Blue));   // b // Blue
-        m.insert('c', boxed!(Cyan));   // c // Cyan
-        m.insert('g', boxed!(Green));  // g // Green
-        m.insert('m', boxed!(Magenta));// m // Magenta
-        m.insert('r', boxed!(Red));    // r // Red
-        m.insert('w', boxed!(White));  // w // White
-        m.insert('y', boxed!(Yellow)); // y // Yellow
-        // Uppercase 'R' resets all colors
-        m.insert('R', boxed!(Reset));  // R // Reset
-        m
-    };
-
-    // Ignore for now?
-    // LightBlack  High-intensity light black.
-    // LightBlue   High-intensity light blue.
-    // LightCyan   High-intensity light cyan.
-    // LightGreen  High-intensity light green.
-    // LightMagenta    High-intensity light magenta.
-    // LightRed    High-intensity light red.
-    // LightWhite  High-intensity light white.
-    // LightYellow High-intensity light yellow.
+    print!("{}", Down(18));
 }
 
 fn main() {
