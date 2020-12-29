b82da05bf886ae6e87131c63a89da94a3b19edd8
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2020-12-29 18:32:11 -0300

    Add Github Actions workflow

---
+++ run-under-valgrind.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Testing Pulga under Valgrind"
+
+# Making sure we're in the directory where this script is contained
+PRG="$0"
+while [ -h "$PRG" ] ; do
+   PRG=`readlink "$PRG"`
+done
+scriptdir=`dirname "$PRG"`
+cd $scriptdir
+
+# Run it under Valgrind, fail if Valgrind reports any error
+if valgrind --leak-check=full --error-exitcode=23 ./target/release/pulga; then
+    echo "Valgrind did not find any errors!"
+else
+    exit $127
+fi
+
