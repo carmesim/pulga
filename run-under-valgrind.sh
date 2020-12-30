e40499127ae9fd35968254a118d438fffd4f7cf6
João M. Bezerra <marcospb19@hotmail.com>
2020-12-30 12:53:39 -0300

    Add script check

--- run-under-valgrind.sh
+++ run-under-valgrind.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "Testing Pulga under Valgrind"
 
 # Making sure we're in the directory where this script is contained
@@ -6,13 +6,22 @@ PRG="$0"
 while [ -h "$PRG" ] ; do
    PRG=$(readlink "$PRG")
 done
+
 scriptdir=$(dirname "$PRG")
 cd $scriptdir
+#
 
-# Run it under Valgrind, fail if Valgrind reports any error
-if valgrind --leak-check=full --error-exitcode=23 ./target/release/pulga; then
-    echo "Valgrind did not find any errors!"
-else
-    exit ${127}
+if [[ ! -f "./target/release/pulga" ]]; then
+    echo "you need to run \"cargo build --release\" first"
+    exit 1
 fi
 
+# Run it under Valgrind, fail if Valgrind reports any error
+valgrind --leak-check=full --error-exitcode=123 ./target/release/pulga
+
+if [[ "$?" != '123' ]]; then
+    echo ''
+    echo 'Valgrind did not find any errors'
+else
+    exit 127
+fi
